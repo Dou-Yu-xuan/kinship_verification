4f083f165d4037ee38ad2017aefbe9805e1fa41b
franchupetro <pfranc@softserveinc.com>
2021-05-30 15:03:36 +0300

    Add models definitions

---
+++ siamese_keras/models/facenet.py
@@ -0,0 +1,12 @@
+from tensorflow.keras.models import load_model
+
+
+def FaceNet():
+    model_path = '../weights/facenet_keras.h5'
+    model = load_model(model_path)
+
+    for layer in model.layers[:-3]:
+        layer.trainable = True
+
+    return model
+
